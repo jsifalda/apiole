b6dc4d7468cb13d0c03fc6aebf0e5ba1834d4b2b
jsifalda <sifalda.jiri@gmail.com>
2015-08-31 13:48:24 +0200

    Ability to pass object as url params

--- src/validators/buildPath.js
+++ src/validators/buildPath.js
@@ -7,16 +7,26 @@ let buildPath = (value) => {
     }
 
     let path = value[0]
-    let counter = 1
-
     if (typeof path !== 'string') {
       throw new Error('First index of array has to be string - endpoint path!')
     }
 
-    path = path.replace(/(:(.[^\/]+))/g, (item) => {
-      counter++
-      return value[counter - 1] || item
-    })
+    if (value.length === 2 && typeof value[1] === 'object') {
+
+      path = path.replace(/(:(.[^\/]+))/g, (item) => {
+        return value[1][item.replace(':', '')] || item
+      })
+
+    } else {
+
+      let counter = 1
+
+      path = path.replace(/(:(.[^\/]+))/g, (item) => {
+        counter++
+        return value[counter - 1] || item
+      })
+
+    }
 
     return path
   }
